4e583bd9e8ab582630483778237c74267ede4196
Fox <foxluck2@mail.ru>
2011-07-18 17:48:18 +0400

    add file

--- main_handler.py
+++ main_handler.py
@@ -1,4 +1,4 @@
-from handlers import base, data, main, service, issue, documents, reference, google_calendar
+from handlers import base, data, exportdata, main, service, issue, documents, reference, google_calendar
 
 if __name__ == "__main__":
     base.start()
